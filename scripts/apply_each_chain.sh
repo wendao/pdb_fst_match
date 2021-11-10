0a684fe724429a49e7aae6d0fb13b396965131a0
wendao <pandawendao@gmail.com>
2021-11-10 10:12:04 +0800

    extract pdb sequence

--- scripts/apply_each_chain.sh
+++ scripts/apply_each_chain.sh
@@ -1,12 +1,13 @@
 for p in $(cat $1)
 do
   folder=`echo $p|cut -c 2-3`
+  pdb_name=`echo $p|awk -F'_' '{print $1 "_" $2}'`
   fst_name=`echo $p|awk -F'_' '{print tolower($1) "_" $2}'`
   echo $p $folder $fst_name
   cd fst/${folder}/
     grep -A 1 ${fst_name} ../../../database/pdb_seqres.txt > ${p}.fasta
   cd ../..
   cd raw/${folder}/
-    
+    ../../../bin/pdb2fasta ${pdb_name}.pdb > ${pdb_name}.pdb.fst
   cd ../..
 done
